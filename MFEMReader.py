2a85b8d64d4db7c873aff54f2358778e882f5bd0
Ben Zwick <benzwick@gmail.com>
2021-05-10 12:49:41 +0800

    Add support for mfem_root data collection files

--- MFEMReader.py
+++ MFEMReader.py
@@ -55,6 +55,7 @@ class MFEMReader(VTKPythonAlgorithmBase):
 
         import os
         import json
+        import mfem.ser as mfem
 
         _, ext = os.path.splitext(self._filename)
         cwd = os.path.dirname(self._filename)
@@ -66,42 +67,25 @@ class MFEMReader(VTKPythonAlgorithmBase):
             with open(self._filename) as f:
                 root = json.load(f)
                 mesh_filename = os.path.join(cwd, root['dsets']['main']['mesh']['path'])
-                # FIXME:
+                # FIXME: create mesh filename using 'cycle'
                 mesh_filename = mesh_filename[:-4] + "000000"
         else:
             raise RuntimeError("Unsupported file format: {}".format(ext))
 
-        with open(mesh_filename, "r") as f:
-            lines = f.readlines()
+        if False: # for interactive development only
+            mesh_filename = "/home/ben/projects/mfem/mfem/data/beam-hex.mesh"
 
-        mesh_format = lines[0].strip()
-        assert mesh_format == "MFEM mesh v1.0", \
-            "Unsupported mesh format: {}".format(mesh_format)
-
-        for i, l in enumerate(lines):
-            if l.startswith('dimension'):
-                dim = int(lines[i+1])
-            elif l.startswith('elements'):
-                nelem = int(lines[i + 1])
-                elem_start = i + 2
-                elem_end = elem_start + nelem
-            elif l.startswith('vertices'):
-                nvert = int(lines[i + 1])
-                vdim = int(lines[i + 2])
-                vert_start = i + 3
-                vert_end = vert_start + nvert
-
-        elem = lines[elem_start:elem_end]
-        vert = lines[vert_start:vert_end]
-        assert len(elem) == nelem
-        assert len(vert) == nvert
-        del lines
+        mesh = mfem.Mesh(mesh_filename)
+        dim = mesh.Dimension()
+        nelem = mesh.GetNE()
+        nvert = mesh.GetNV()
 
         # Points
-        points = np.array([np.array(v.split(), dtype=float) for v in vert])
-        if dim == 2:
+        points = mesh.GetVertexArray()
+        if dim == 1:
+            points = np.hstack([points, np.zeros((nvert, 2))])
+        elif dim == 2:
             points = np.hstack([points, np.zeros((nvert, 1))])
-        del vert
         output.SetPoints(points)
         del points
 
@@ -112,16 +96,16 @@ class MFEMReader(VTKPythonAlgorithmBase):
         cell_conn = []
 
         offset = 0
-        for i, e in enumerate(elem):
-            s = [int(x) for x in e.split()]
-            a, t, v = s[0], s[1], s[2:]
+        for i in range(nelem):
+            a = mesh.GetAttribute(i)
+            t = mesh.GetElementType(i)
+            v = mesh.GetElementVertices(i)
             cell_attributes[i] = a
             cell_types[i] = mfem_to_vtk_type[t]
             cell_offsets[i] = offset
             offset += len(v) + 1
             cell_conn.append(len(v))
             cell_conn.extend(v)
-        del elem
 
         output.SetCells(cell_types, cell_offsets, cell_conn)
         del cell_types, cell_offsets, cell_conn
@@ -133,27 +117,17 @@ class MFEMReader(VTKPythonAlgorithmBase):
         if ext == ".mfem_root":
             fields = root['dsets']['main']['fields']
             for name, prop in fields.items():
+                filename = os.path.join(cwd, prop['path'])
+                # FIXME: create field filename using 'cycle'
+                filename = filename[:-4] + "000000"
+                data = mfem.GridFunction(mesh, filename).GetDataArray()
                 if prop['tags']['assoc'] == 'nodes':
-                    filename = os.path.join(cwd, prop['path'])
-                    filename = filename[:-4] + "000000"
-                    with open(filename) as f:
-                        # TODO: support more FE spaces etc.
-                        lines = f.readlines()
-                        fespace = lines[0].strip()
-                        assert fespace == "FiniteElementSpace"
-                        fec = lines[1].strip()
-                        assert fec == "FiniteElementCollection: H1_{}D_P1".format(dim)
-                        vdim = int(lines[2].split(':')[1].strip())
-                        ordering = int(lines[3].split(':')[1].strip())
-                        # empty line between header and data
-                        assert lines[4].strip() == ''
-                        data = np.array([np.array(l.split(), dtype=float) for l in lines[5:]])
-                        assert data.shape[0] == nvert
-                        assert data.shape[1] == vdim
-                    del lines
                     output.PointData.append(data, name)
+                    del data
+                if prop['tags']['assoc'] == 'elements':
+                    output.CellData.append(data, name)
+                    del data
                 else:
-                    # TODO: add element data
                     raise NotImplementedError("assoc: '{}'".format(prop['tags']['assoc']))
 
         return 1
--- README.md
+++ README.md
@@ -1,7 +1,66 @@
 # ParaView MFEM Reader
 
-ParaView plugin for reading [MFEM](https://mfem.org) mesh files.
+ParaView plugin for reading [MFEM](https://mfem.org) files.
 
-Currently only the
-[MFEM mesh v1.0](https://mfem.org/mesh-formats/#mfem-mesh-v10)
-mesh format is supported.
+This plugin can be used to read MFEM `.mesh` and
+`.mfem_root` (VisItDataCollection) files.
+
+## Installing
+
+This plugin requires [PyMFEM](https://github.com/mfem/PyMFEM)
+to be installed within the ParaView Python environment.
+Please see the references below for additional details.
+
+1. Set variables:
+
+        export PV_PY_VER=3.7.4
+        export PV_NP_VER=1.16.4
+        export PV_SIX_VER=1.11.0
+        export PV_PY_PREFIX=/opt/paraview/pvpy-${PV_PY_VER}
+        export PV_PY_VENV=${PV_PY_PREFIX}-venv
+
+2. Install Python:
+
+        wget https://www.python.org/ftp/python/${PV_PY_VER}/Python-${PV_PY_VER}.tgz
+        tar xzvf Python-${PV_PY_VER}.tgz
+        cd Python-${PV_PY_VER}
+        ./configure --enable-optimizations --prefix=${PV_PY_PREFIX}
+        make
+        make install
+        cd ..
+
+3. Create virtual environment:
+
+        ${PV_PY_PREFIX}/bin/python3 -m venv ${PV_PY_VENV}
+
+4. Install packages into virtual environment:
+
+        source pvpy-${PV_PY_VER}-venv/bin/activate
+        pip uninstall -y numpy
+        pip install --ignore-installed numpy==$PV_NP_VER
+        pip uninstall -y six
+        pip install --ignore-installed six==$PV_SIX_VER
+        pip install mfem
+
+5. Before starting ParaView (note that python3.7 is used here):
+
+        export PYTHONPATH=${PV_PY_VENV}/lib/python3.7/site-packages
+
+6. Load the `MFEMReader.py` module in ParaView
+   via `Tools` &rarr; `Manage Plugins` &rarr; `Load New`.
+   You can optionally check the `Auto Load` option.
+
+You should now be able to open MFEM files using `File` &rarr; `Open`.
+
+## References
+
+- <https://github.com/DaanVanVugt/paraview-python-file-reader>
+- <https://www.litianyi.me/2019/10/27/paraview-meshio-plugin/>
+- <https://github.com/tianyikillua/paraview-dolfinx-reader>
+- <https://github.com/nschloe/meshio/blob/main/tools/paraview-meshio-plugin.py>
+- <https://blog.kitware.com/easy-customization-of-the-paraview-python-programmable-filter-property-panel>
+- <https://www.paraview.org/Wiki/Python_calculator_and_programmable_filter>
+- <https://public.kitware.com/pipermail/paraview-developers/2017-January/005051.html>
+- <https://discourse.paraview.org/t/how-can-i-install-and-import-other-modules-inside-pvpython/3067/6>
+- <https://gitlab.kitware.com/paraview/paraview/-/issues/17891>
+- <https://docs.python.org/3/library/venv.html>
