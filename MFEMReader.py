f89e0cdfc72fa72c099d495eca39474cec6e72c2
Ben Zwick <benzwick@gmail.com>
2021-05-10 17:40:58 +0800

    Start adding support for curved meshes

--- MFEMReader.py
+++ MFEMReader.py
@@ -15,16 +15,18 @@ __copyright__ = "Copyright (c) 2021 {} <{}>".format(__author__, __email__)
 __license__ = "License :: OSI Approved :: BSD-3-Clause License"
 __version__ = "0.1.0"
 
+# VTK element type based on MFEM geometry and nodes FE space order
 # MFEM: https://github.com/mfem/mfem/blob/master/fem/geom.hpp
 # VTK:  https://vtk.org/doc/nightly/html/vtkCellType_8h_source.html
+# NOTE: Linear elements load much faster than arbitrary order Lagrange elements
 mfem_to_vtk_type = [
-    1,  # POINT = 0
-    3,  # SEGMENT = 1
-    5,  # TRIANGLE = 2
-    9,  # SQUARE = 3
-    10, # TETRAHEDRON = 4
-    12, # CUBE = 5
-    13, # PRISM = 6
+    [ 1,  1,  1],  # POINT = 0
+    [-1,  3, 68], # SEGMENT = 1
+    [-1,  5, 69], # TRIANGLE = 2
+    [-1,  9, 70], # SQUARE = 3
+    [-1, 10, 71], # TETRAHEDRON = 4
+    [-1, 12, 72], # CUBE = 5
+    [-1, 13, 73], # PRISM = 6
 ]
 
 extensions = [
@@ -83,14 +85,31 @@ class MFEMReader(VTKPythonAlgorithmBase):
         mesh = mfem.Mesh(mesh_filename)
         dim = mesh.Dimension()
         nelem = mesh.GetNE()
-        nvert = mesh.GetNV()
+
+        # FIXME: read field grid functions first to determine the correct order
+        #        to use for the mesh curvature.
 
         # Points
-        points = mesh.GetVertexArray()
+        if mesh.GetNodes() is None:
+            mesh_is_curved = False
+            nnode = mesh.GetNV()
+            points = mesh.GetVertexArray()
+        else:
+            print("WARNING: Reading curved meshes is untested and probably broken!")
+            mesh_is_curved = True
+            nnode = mesh.GetNodes().Size() // dim
+            points = mesh.GetNodes().GetDataArray()
+            mesh_fes = mesh.GetNodalFESpace()
+            if mesh_fes.GetOrdering() == 0:
+                points = points.reshape((dim, nnode)).T
+            elif mesh_fes.GetOrdering() == 1:
+                points = points.reshape((nnode, dim))
+            else:
+                raise NotImplementedError
         if dim == 1:
-            points = np.hstack([points, np.zeros((nvert, 2))])
+            points = np.hstack([points, np.zeros((nnode, 2))])
         elif dim == 2:
-            points = np.hstack([points, np.zeros((nvert, 1))])
+            points = np.hstack([points, np.zeros((nnode, 1))])
         output.SetPoints(points)
         del points
 
@@ -104,9 +123,18 @@ class MFEMReader(VTKPythonAlgorithmBase):
         for i in range(nelem):
             a = mesh.GetAttribute(i)
             t = mesh.GetElementType(i)
-            v = mesh.GetElementVertices(i)
+            if mesh_is_curved:
+                # FIXME: MFEM ordering is different to VTK
+                v = mesh_fes.GetElementDofs(i)
+            else:
+                v = mesh.GetElementVertices(i)
             cell_attributes[i] = a
-            cell_types[i] = mfem_to_vtk_type[t]
+            if mesh_is_curved:
+                elem_order = mesh_fes.GetOrder(i)
+            else:
+                elem_order = 1
+            # FIXME: add support for mesh order > 1
+            cell_types[i] = mfem_to_vtk_type[t][min(elem_order, 2)]
             cell_offsets[i] = offset
             offset += len(v) + 1
             cell_conn.append(len(v))
@@ -125,11 +153,17 @@ class MFEMReader(VTKPythonAlgorithmBase):
                 filename = os.path.join(cwd, prop['path'])
                 # FIXME: create field filename using 'cycle'
                 filename = filename[:-4] + "000000"
-                data = mfem.GridFunction(mesh, filename).GetDataArray()
+                gf = mfem.GridFunction(mesh, filename)
+                vdim = gf.VectorDim()
+                # FIXME: add support for vector fields which will required reshape
+                #        of data based on ordering similar to nodes.
+                if vdim != 1:
+                    raise NotImplementedError("Only scalar fields (VDim == 1) are allowed.")
+                data = gf.GetDataArray()
                 if prop['tags']['assoc'] == 'nodes':
                     output.PointData.append(data, name)
                     del data
-                if prop['tags']['assoc'] == 'elements':
+                elif prop['tags']['assoc'] == 'elements':
                     output.CellData.append(data, name)
                     del data
                 else:
