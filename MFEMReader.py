7bf75a931aef843be19a85da90f9f75e9f63be9a
Ben Zwick <benzwick@gmail.com>
2021-05-21 16:48:31 +0800

    Add support for quadratic elements

--- MFEMReader.py
+++ MFEMReader.py
@@ -8,26 +8,13 @@ from paraview.util.vtkAlgorithm import (
 )
 from vtkmodules.numpy_interface import dataset_adapter as dsa
 from vtkmodules.vtkCommonDataModel import vtkUnstructuredGrid
+import vtk
 
 __author__ = "Benjamin F. Zwick"
 __email__ = "benzwick@gmail.com"
 __copyright__ = "Copyright (c) 2021 {} <{}>".format(__author__, __email__)
 __license__ = "License :: OSI Approved :: BSD-3-Clause License"
 __version__ = "0.1.0"
-
-# VTK element type based on MFEM geometry and nodes FE space order
-# MFEM: https://github.com/mfem/mfem/blob/master/fem/geom.hpp
-# VTK:  https://vtk.org/doc/nightly/html/vtkCellType_8h_source.html
-# NOTE: Linear elements load much faster than arbitrary order Lagrange elements
-mfem_to_vtk_type = [
-    [ 1,  1,  1], # POINT = 0
-    [-1,  3, 68], # SEGMENT = 1
-    [-1,  5, 69], # TRIANGLE = 2
-    [-1,  9, 70], # SQUARE = 3
-    [-1, 10, 71], # TETRAHEDRON = 4
-    [-1, 12, 72], # CUBE = 5
-    [-1, 13, 73], # PRISM = 6
-]
 
 extensions = [
     # MFEM collection
@@ -42,6 +29,59 @@ extensions = [
 ]
 
 file_description = "MFEM files"
+
+# VTK element type based on MFEM geometry and nodes FE space order
+# MFEM: - https://github.com/mfem/mfem/blob/master/fem/geom.hpp
+#       - https://github.com/mfem/mfem/blob/master/mesh/vtk.cpp
+# VTK:  - https://vtk.org/doc/nightly/html/vtkCellType_8h_source.html
+# NOTE: Linear elements load much faster than arbitrary order Lagrange elements
+
+VTKGeometry_Map = [
+    vtk.VTK_VERTEX,                       # POINT
+    vtk.VTK_LINE,                         # SEGMENT
+    vtk.VTK_TRIANGLE,
+    vtk.VTK_QUAD,                         # SQUARE
+    vtk.VTK_TETRA,                        # TETRAHEDRON
+    vtk.VTK_HEXAHEDRON,                   # CUBE
+    vtk.VTK_WEDGE]                        # PRISM
+
+VTKGeometry_QuadraticMap = [
+    vtk.VTK_VERTEX,                       # POINT
+    vtk.VTK_QUADRATIC_EDGE,               # SEGMENT
+    vtk.VTK_QUADRATIC_TRIANGLE,
+    vtk.VTK_BIQUADRATIC_QUAD,             # SQUARE
+    vtk.VTK_QUADRATIC_TETRA,              # TETRAHEDRON
+    vtk.VTK_TRIQUADRATIC_HEXAHEDRON,      # CUBE
+    vtk.VTK_BIQUADRATIC_QUADRATIC_WEDGE]  # PRISM
+
+VTKGeometry_HighOrderMap = [
+    vtk.VTK_VERTEX,                       # POINT
+    vtk.VTK_LAGRANGE_CURVE,               # SEGMENT
+    vtk.VTK_LAGRANGE_TRIANGLE,
+    vtk.VTK_LAGRANGE_QUADRILATERAL,       # SQUARE
+    vtk.VTK_LAGRANGE_TETRAHEDRON,
+    vtk.VTK_LAGRANGE_HEXAHEDRON,          # CUBE
+    vtk.VTK_LAGRANGE_WEDGE]               # PRISM
+
+VTKGeometry_PrismMap = [0, 2, 1, 3, 5, 4]
+
+VTKGeometry_VertexPermutation = [
+    None, None, None, None, None, None, VTKGeometry_PrismMap]
+
+# https://github.com/mfem/mfem/blob/master/mesh/mesh_readers.cpp
+Mesh_vtk_quadratic_tet = [
+    0, 1, 2, 3, 4, 7, 5, 6, 8, 9]
+Mesh_vtk_quadratic_wedge = [
+    0, 2, 1, 3, 5, 4, 8, 7, 6, 11, 10, 9, 12, 14, 13, 17, 16, 15]
+Mesh_vtk_quadratic_hex = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    24, 22, 21, 23, 20, 25, 26]
+
+VTKGeometry_QuadraticVertexPermutation = [
+    None, None, None, None,
+    Mesh_vtk_quadratic_tet,
+    Mesh_vtk_quadratic_hex,
+    Mesh_vtk_quadratic_wedge]
 
 @smproxy.reader(
     label="MFEM reader",
@@ -84,11 +124,8 @@ class MFEMReader(VTKPythonAlgorithmBase):
             mesh_filename = self._filename
 
         mesh = mfem.Mesh(mesh_filename)
-        dim = mesh.Dimension()
+        dim = mesh.SpaceDimension()
         nelem = mesh.GetNE()
-
-        # FIXME: read field grid functions first to determine the correct order
-        #        to use for the mesh curvature.
 
         # Points
         nodes = mesh.GetNodes()
@@ -96,7 +133,6 @@ class MFEMReader(VTKPythonAlgorithmBase):
             nnode = mesh.GetNV()
             points = np.array(mesh.GetVertexArray())
         else:
-            print("WARNING: Reading curved meshes is untested and probably broken!")
             nnode = mesh.GetNodes().Size() // dim
             points = np.array(mesh.GetNodes().GetDataArray())
             mesh_fes = mesh.GetNodalFESpace()
@@ -121,25 +157,38 @@ class MFEMReader(VTKPythonAlgorithmBase):
         # Element order for each mesh element
         elem_order = np.ones((nelem), dtype=int)
 
-        # Function used to get element nodal connectivity
-        if nodes is None:
-            get_conn = mesh.GetElementVertices
-        else:
-            # FIXME: MFEM ordering is different to VTK
-            get_conn = mesh_fes.GetElementDofs
-            for i in range(nelem):
-                elem_order[i] = mesh_fes.GetOrder(i)
-
         offset = 0
-        for i in range(nelem):
-            t = mesh.GetElementType(i)
-            v = get_conn(i)
-            # FIXME: add support for mesh order > 1
-            cell_types[i] = mfem_to_vtk_type[t][min(elem_order[i], 2)]
-            cell_offsets[i] = offset
-            offset += len(v) + 1
-            cell_conn.append(len(v))
-            cell_conn.extend(v)
+        if nodes is None:
+            for i in range(nelem):
+                v = mesh.GetElementVertices(i)
+                geom = mesh.GetElementBaseGeometry(i)
+                perm = VTKGeometry_VertexPermutation[geom]
+                if perm: v = [v[i] for i in perm]
+                cell_types[i] = VTKGeometry_Map[geom]
+                cell_offsets[i] = offset
+                offset += len(v) + 1
+                cell_conn.append(len(v))
+                cell_conn.extend(v)
+        else:
+            for i in range(nelem):
+                v = mesh_fes.GetElementDofs(i)
+                geom = mesh.GetElementBaseGeometry(i)
+                order = mesh_fes.GetOrder(i)
+                if order == 1:
+                    perm = VTKGeometry_VertexPermutation[geom]
+                    cell_types[i] = VTKGeometry_Map[geom]
+                elif order == 2:
+                    perm = VTKGeometry_QuadraticVertexPermutation[geom]
+                    cell_types[i] = VTKGeometry_QuadraticMap[geom]
+                else:
+                    print(f"Failed to read mesh file {mesh_filename}")
+                    print(f"ERROR: Only elements of order 1 and 2 are supported")
+                    return 1
+                if perm: v = [v[i] for i in perm]
+                cell_offsets[i] = offset
+                offset += len(v) + 1
+                cell_conn.append(len(v))
+                cell_conn.extend(v)
 
         output.SetCells(cell_types, cell_offsets, cell_conn)
 
